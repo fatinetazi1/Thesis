d039ffeba4b0c0eea8c669147beab47c87dec9f3
Fatine Tazi <f.tazi@jacobs-university.de>
2020-05-31 02:14:33 +0200

    Change of method: using feature extraction

--- Demo Stereo Stage 0.cpp
+++ Demo Stereo Stage 0.cpp
@@ -1,124 +1,142 @@
 #include "DGM.h"
 #include "DGM/timer.h"
-
 #include "PFM.h"
 
 using namespace DirectGraphicalModels;
 
-void print_help(char *argv0) {
-    printf("Usage: %s left_image right_image min_disparity max_disparity right_image_groundtruth output_disparity\n", argv0);
+void print_help(char* argv0) {
+    printf("Usage: %s left_image right_image min_disparity max_disparity right_image_groundtruth image_features_folder output_disparity\n", argv0);
 }
 
-float meanAbs(Mat solution, Mat gt, float scaleFactor) {
+float meanAbs(Mat solution, Mat gt) {
     float sum = 0;
     for (int y = 0; y < solution.rows; y++) {
-        const byte *pM1 = solution.ptr<byte>(y);
-        const byte *pM2 = gt.ptr<byte>(y);
-        for (int x = 0; x < solution.cols; x++){
-            float res = static_cast<float>(pM1[x]);
-            float gt = static_cast<float>(pM2[x]);
-            float percentage = gt ? abs(res - gt)/gt : abs(res - gt);
+        const byte* pSolution = solution.ptr<byte>(y);
+        const byte* pGT = gt.ptr<byte>(y);
+        for (int x = 0; x < solution.cols; x++) {
+            float solution_val = static_cast<float>(pSolution[x]);
+            float gt_val = static_cast<float>(pGT[x]);
+            float percentage = (solution_val - gt_val) * (solution_val - gt_val);
             sum += percentage;
         }
     }
-    return sum / (solution.rows*solution.cols);
+    return sum / (solution.rows * solution.cols);
 }
 
-float badPixel(Mat solution, Mat gt, float scaleFactor) {
-    float threshold = 1;
+float badPixel(Mat solution, Mat gt) {
+    const float threshold = 1;
     float sum = 0;
     for (int y = 0; y < solution.rows; y++) {
-        const byte *pM1 = solution.ptr<byte>(y);
-        const byte *pM2 = gt.ptr<byte>(y);
-        for (int x = 0; x < solution.cols; x++){
-            float res = static_cast<float>(pM1[x]);
-            float gt = static_cast<float>(pM2[x]);
-            float difference = abs(res - gt);
-            if (difference > threshold) sum++;
+        const byte* pSolution = solution.ptr<byte>(y);
+        const byte* pGT = gt.ptr<byte>(y);
+        for (int x = 0; x < solution.cols; x++) {
+            float solution_val = static_cast<float>(pSolution[x]);
+            float gt_val = static_cast<float>(pGT[x]);
+            float difference = fabs(solution_val - gt_val);
+            if (difference >= threshold) sum++;
         }
     }
-    return sum / (solution.rows*solution.cols);
+    return 100 * sum / (solution.rows * solution.cols);
 }
 
-int main(int argc, char *argv[]) {
-    
-    if (argc != 7) {
+int main(int argc, char* argv[]) {
+
+    if (argc != 8) {
         print_help(argv[0]);
         return 0;
     }
-    
-    const int gtScaleFactor = 6;
 
     // Reading parameters and images
     Mat imgL = imread(argv[1], 0);          if (imgL.empty()) printf("Can't open %s\n", argv[1]);
-    resize(imgL, imgL, Size(imgL.cols / gtScaleFactor, imgL.rows / gtScaleFactor));
-    
     Mat imgR = imread(argv[2], 0);          if (imgR.empty()) printf("Can't open %s\n", argv[2]);
-    int rows = imgR.rows;
-    int cols = imgR.cols;
-    resize(imgR, imgR, Size(imgR.cols / gtScaleFactor, imgR.rows / gtScaleFactor));
     
-    
-    int       minDisparity  = atoi(argv[3]);
-    int       maxDisparity  = atoi(argv[4]);
-    int       width         = imgL.cols;
-    int       height        = imgL.rows;
-    unsigned int nStates    = maxDisparity - minDisparity;
-    
-    const int assertScaleFactor   = 255/nStates;
-    
+    int       minDisparity = atoi(argv[3]);
+    int       maxDisparity = atoi(argv[4]);
+    unsigned int nStates = maxDisparity - minDisparity;
+
     PFM imgR_pfm;
     float* pimgR_pfm = imgR_pfm.read_pfm<float>(argv[5]);
-    Mat imgR_gt = Mat(rows, cols, CV_32FC1, pimgR_pfm);
-    resize(imgR_gt, imgR_gt, Size(imgR_gt.cols / gtScaleFactor, imgR_gt.rows / gtScaleFactor));
+    Mat imgR_gt = Mat(imgR.size(), CV_32FC1, pimgR_pfm);
     
+    // Scaling
+    const int gtScaleFactor = 6;
+    resize(imgL, imgL, Size(imgL.cols / gtScaleFactor, imgL.rows / gtScaleFactor)); //byte
+    resize(imgR, imgR, Size(imgR.cols / gtScaleFactor, imgR.rows / gtScaleFactor)); //byte
+    resize(imgR_gt, imgR_gt, Size(imgR_gt.cols / gtScaleFactor, imgR_gt.rows / gtScaleFactor)); //floating pont
+
+    int       height = imgL.rows;
+    int       width = imgL.cols;
+    
+    // Extracted SIFT Features
+    const int features = 128;
+    Mat fexL[features];
+    Mat fexR[features];
+    for (size_t ch = 0; ch < features; ch++) {
+        fexL[ch] = imread(std::string(argv[6]) + std::string("fexL") + std::to_string(ch) +  std::string(".png"));
+        fexR[ch] = imread(std::string(argv[6]) + std::string("fexR") + std::to_string(ch) +  std::string(".png"));
+    }
+
     CGraphPairwiseKit graphKit(nStates, INFER::TRW);
-//    CGraphDenseKit graphKit(nStates); // Uncomment for Dense graph model, comment line above.
-    
+
     graphKit.getGraphExt().buildGraph(imgL.size());
     graphKit.getGraphExt().addDefaultEdgesModel(1.175f);
-    
+
     // ==================== Filling the nodes of the graph ====================
     Mat nodePot(nStates, 1, CV_32FC1);                                      // node Potential (column-vector)
     size_t idx = 0;
     for (int y = 0; y < height; y++) {
-        byte * pImgL    = imgL.ptr<byte>(y);
-        byte * pImgR    = imgR.ptr<byte>(y);
+        byte* pImgL = imgL.ptr<byte>(y);
+        byte* pImgR = imgR.ptr<byte>(y);
         for (int x = 0; x < width; x++) {
             float imgL_value = static_cast<float>(pImgL[x]);
             for (unsigned int s = 0; s < nStates; s++) {                    // state
                 int disparity = minDisparity + s;
                 float imgR_value = (x + disparity < width) ? static_cast<float>(pImgR[x + disparity]) : imgL_value;
-                float p = 1.0f - fabs(imgL_value - imgR_value) / 255.0f;
+                float p1 = 1.0f - fabs(imgL_value - imgR_value) / 255.0f;
+                float sum = 0;
+                for (size_t ch = 0; ch < features; ch++) {
+                    byte* pFexL = fexL[ch].ptr<byte>(y);
+                    byte* pFexR = fexR[ch].ptr<byte>(y);
+                    float val_l = static_cast<float>(pFexL[x]);
+                    float val_r = static_cast<float>(pFexR[x]);
+                    sum += fabs(val_l - val_r) / 255;
+                }
+                float p2 = 1.0f - sum / features;
+                float p = p1 * p2;
                 nodePot.at<float>(s, 0) = p * p;
             }
             graphKit.getGraph().setNode(idx++, nodePot);
         } // x
     } // y
-    
+
     // =============================== Decoding ===============================
     Timer::start("Decoding... ");
     vec_byte_t optimalDecoding = graphKit.getInfer().decode(10);
     Timer::stop();
-    
+
     // ============================ Evaluation =============================
-    Mat disparity(imgL.size(), CV_8UC1, optimalDecoding.data());
-    float meanError = meanAbs(disparity, imgR_gt, assertScaleFactor);
-    float badError = badPixel(disparity, imgR_gt,  assertScaleFactor);
-    
+    Mat disparity(imgL.size(), CV_8UC1, optimalDecoding.data());  // the values are [0; nStates] = [0; maxDisp - minDisp]
+    disparity = disparity + minDisparity;
+    Mat gt;                                                        // the values are [minDisp; maxDisp]
+    imgR_gt.convertTo(gt, CV_8UC1, 1.0 / gtScaleFactor);
+
+    float meanError = meanAbs(disparity, gt);
+    float badError = badPixel(disparity, gt);
+
     // ============================ Visualization =============================
-    disparity = (disparity + minDisparity) * (256 / maxDisparity);
+    disparity = disparity * (256 / maxDisparity);
+    //disparity = disparity * gtScaleFactor;
     medianBlur(disparity, disparity, 3);
-    
+
     char error_str[255];
-    sprintf(error_str, "Mean abs error: %.2f | Bad pixel: %.2f", meanError, badError);
-    putText(disparity, error_str, Point(width - 300, height - 25), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 0.45, Scalar(0, 0, 0), 1, cv::LineTypes::LINE_AA);
-    
+    sprintf(error_str, "MSE: %.2f | Bad pixel percentage: %.2f %%", meanError, badError);
+    putText(disparity, error_str, Point(width - 320, height - 25), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 0.45, Scalar(0, 0, 0), 1, cv::LineTypes::LINE_AA);
+
     char disp_str[255];
     sprintf(disp_str, "Min-disparity: %d | Max-disparity: %d", minDisparity, maxDisparity);
     putText(disparity, disp_str, Point(width - 290, height - 5), cv::HersheyFonts::FONT_HERSHEY_SIMPLEX, 0.45, Scalar(0, 0, 0), 1, cv::LineTypes::LINE_AA);
-    
-    imwrite(argv[6], disparity);
+
+    imwrite(argv[7], disparity);
     imshow("Disparity", disparity);
     waitKey();
 
